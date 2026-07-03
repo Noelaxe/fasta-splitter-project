57d8290fc47a86f5e51b1f7ff24bd321e0a952bc
Noelaxe <noelsjv@gmail.com>
2026-07-03 17:57:47 +0530

    gh-changes

--- FSplitter-project/vite.config.js
+++ FSplitter-project/vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
     tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
   ],
-  base: '/fasta-splitter-project/FSplitter-project'
+  base: '/fasta-splitter-project'
 })
