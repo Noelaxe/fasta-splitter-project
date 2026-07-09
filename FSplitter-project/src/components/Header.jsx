fc3ca3986073571020d48868c6f082717abc1f4e
Noelaxe <noelsjv@gmail.com>
2026-07-09 11:08:10 +0530

    Header change

--- FSplitter-project/src/components/Header.jsx
+++ FSplitter-project/src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return(
         <div>
-            <h1 className="font-sans font-bold text-[56px] text-slate-900 text-center mb-1">FASTA Splitter</h1>
+            <h1 className="font-sans font-bold text-[56px] text-slate-900 text-center mb-1">FASTA File Splitter</h1>
             <p className="font-sans font-semibold text-[20px] text-green-600 text-center">Split large FASTA files into smaller parts with a fixed number of sequences in each</p>
         </div>
     )
